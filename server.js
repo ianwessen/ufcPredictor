aa7cb93400d71269612c39c17d3c10c9c0a4491f
Cody Rioux <crioux@netflix.com>
2016-11-27 13:04:03 -0800

    Implemented some functionality to compute the highest weight (rounded up) passed into the fight.
    Implemented but forgot to test a curry and compose function.
    Users can now pass weightA and weightB as url parameters via
    http://localhost:8080?weightA=144.5&weightB=143.25 to compute
    the highest weight participating in the bout.
    
    Code (probably) contains some suspicious linter violations as well.

--- server.js
+++ server.js
@@ -1,9 +1,25 @@
 var http = require('http');
+var url = require('url');
 
 const PORT = 8080
 
+// TODO: These aren't tested!
+let compose = (...funcs) => (value) => funcs.reduce((v,fn) => fn(v), value);
+let curry = (fun, arg) => (...args) => fun(...[arg, ...args]);
+
 function handleRequest (request,response) {
-	response.end('It works! Path hit: ' + request.url);
+
+    // TODO: I probably have some linter violations here.
+	var requestParams = url.parse(request.url, true).query
+	var weightA = parseFloat(requestParams["weightB"])
+	var weightB = parseFloat(requestParams["weightA"])
+	var maxWeightFinder = compose(curry(Math.max, weightA), Math.ceil)
+
+	response.end(JSON.stringify({
+	    "weightA": weightA,
+	    "weightB": weightB,
+	    "heaviestRoundedUp": maxWeightFinder(weightB)
+	}));
 }
 
 var server = http.createServer(handleRequest)
