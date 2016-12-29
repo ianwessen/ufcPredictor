10ac094a4252ae4eb4b68a83abef4cedc679cc28
ianwessen <ianwessen@gmail.com>
2016-12-28 19:46:50 -0800

    messing around with forms

--- server.js
+++ server.js
@@ -1,12 +1,32 @@
-const express = require('express');
-const app = express();
+const express = require('express'),
+  app = express(),
+  bodyParser = require('body-parser');
 
 const PORT = 8080
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use(express.static('public'));
 
 app.get('/', function(req, res) {
   res.sendFile(__dirname + '/public/app.html');
+});
+
+app.post('/form', function(req, res) {
+  const weightA = req.body.weightA
+  const weightB = req.body.weightB
+  const maxWeightFinder = compose(curry(Math.max, weightA), Math.ceil);
+
+  const maxWeight = JSON.stringify({
+    "weightA": weightA,
+    "weightB": weightB,
+    "heaviestRoundedUp": maxWeightFinder(weightB)
+  })
+  
+  console.log(maxWeight);
+  res.send(maxWeight);
+  
 });
 
 app.listen(PORT, function() {
