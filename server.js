8a30c0b5a0b9e0611f864e8a67d567c8170ae28f
ianwessen <ianwessen@gmail.com>
2017-02-22 15:41:52 -0800

    puttin' a front end on this thing

--- server.js
+++ server.js
@@ -1,53 +1,39 @@
 const express = require('express'),
   app = express(),
-  bodyParser = require('body-parser');
+  bodyParser = require('body-parser')
+  R = require('ramda');
 
 const PORT = 8080
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.use(express.static('public'));
+app.use(express.static('.'));
 
 app.get('/', function(req, res) {
-  res.sendFile(__dirname + '/public/app.html');
+  res.sendFile(__dirname + '/index.html');
 });
 
-app.post('/form', function(req, res) {
+app.post('/prediction', function(req, res) {
+
   const compose = (...funcs) => (value) => funcs.reduce((v,fn) => fn(v), value);
   const curry = (fun, arg) => (...args) => fun(...[arg, ...args]);
   
   const weightA = req.body.weightA;
   const weightB = req.body.weightB;
+  
   const maxWeightFinder = compose(curry(Math.max, weightA), Math.ceil);
+
+  const winningWeight = maxWeightFinder(weightB)
+  const winner = R.invertObj(req.body)[winningWeight]
 
   if (weightA == weightB) {
     res.send("It's a tie!");
   } else {
-    res.send("Winner: " + maxWeightFinder(weightB));
+    res.send(winner == "weightA" ? "Fighter A" : "Fighter B");
   }
-  
 });
 
 app.listen(PORT, function() {
   console.log('server listening on port ' + PORT);
 });
-
-// function handleRequest (request,response) {
-// 
-//     // TODO: I probably have some linter violations here.
-//   const requestParams = url.parse(request.url, true).query
-//   const weightA = parseFloat(requestParams["weightB"])
-//   const weightB = parseFloat(requestParams["weightA"])
-//   const maxWeightFinder = compose(curry(Math.max, weightA), Math.ceil)
-// 
-//   response.end(JSON.stringify({
-//       "weightA": weightA,
-//       "weightB": weightB,
-//       "heaviestRoundedUp": maxWeightFinder(weightB)
-//   }));
-// }
-// 
-// http.createServer(handleRequest).listen(PORT, function () {
-//   console.log('Server listening on: http://localhost:%s', PORT)
-// });
