4ee29f98b23bc2db3a3c67f91f537b78f8852c6a
ianwessen <ianwessen@gmail.com>
2017-02-27 12:47:07 -0800

    file restructuring to sync

--- test/test.js
+++ test/test.js
@@ -1,23 +1,27 @@
-const util = require('../util.js'), 
-  chai = require('chai'), 
+const chai = require('chai'),
   expect = chai.expect;
 
+const utility = require('../app/utility.js');
+
 describe('curry', function() {
-    
+  
   it('should return a function', function() {
     const add = function(a,b) { return a + b };
-    expect(util.curry(add)).to.be.a('function');
+    expect(utility.curry(add)).to.be.a('function');
   });
   
   it('should throw an error if there is no valid function provided as an argument', function() {
-    expect(util.curry()).to.throw(TypeError);
+    expect(utility.curry()).to.throw(TypeError);
   });
+  
 });
 
 describe('compose', function() {
+  
   it('should return a function', function() {
     const add = function(a,b) { return a + b };
     const average = function(sum) { return sum / 2 };
-    expect(util.compose(add,average)).to.be.a('function');
+    expect(utility.compose(add,average)).to.be.a('function');
   });
+  
 });
